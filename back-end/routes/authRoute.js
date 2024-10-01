6499f92aa82c669fb466aec2eff06324b3bceca0
Percy256 <semagandatrevour@gmail.com>
2024-10-01 14:16:53 +0300

    Initial changes in back-end

--- back-end/routes/authRoute.js
+++ back-end/routes/authRoute.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const AuthController = require('../Controllers/authController');
+const express = require("express"); // Importing the express module
+const AuthController = require("../Controllers/authController");
 
 const route = express.Router();
 
-route.post('/auth/signup', AuthController.SignUp);
-route.post('/auth/signin', AuthController.SignIn);
+route.post("/auth/signup", AuthController.SignUp);
+route.post("/auth/signin", AuthController.SignIn);
 
 module.exports = route;
