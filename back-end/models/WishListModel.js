c980c467c2d16ff73607bafed5c831c2d57aec93
Kisejjere Rashid <rashidkisejjere0784@gmail.com>
2024-10-03 22:22:58 +0300

    fixed eslint

--- back-end/models/WishListModel.js
+++ back-end/models/WishListModel.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
 const wishListSchema = new Schema({
-  product_id: { type: Schema.Types.ObjectId, ref: "Product", required: true },
-  user_id: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  product_id: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+  user_id: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
 });
 
-const wishList = mongoose.model("wishList", wishListSchema);
+const wishList = mongoose.model('wishList', wishListSchema);
 
 module.exports = wishList;
