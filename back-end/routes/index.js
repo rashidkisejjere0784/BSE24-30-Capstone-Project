1b64aa8005905ce7ce83d655c6bfd99adc5edc40
Percy256 <semagandatrevour@gmail.com>
2024-10-03 15:15:08 +0300

    Solved ESLint errors and Added the Wishlist Backend Code functionality

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const authRouter = require('./authRoute.js');
-const productRouter = require('./productRoute.js');
-const brandRouter = require('./brandRoute.js');
-const categoryRouter = require('./categoryRoute.js');
-const productImageRouter = require('./productImageRoute.js');
-const cartRouter = require('./cartRoute.js');
+const express = require("express");
+const authRouter = require("./authRoute.js");
+const productRouter = require("./productRoute.js");
+const brandRouter = require("./brandRoute.js");
+const categoryRouter = require("./categoryRoute.js");
+const productImageRouter = require("./productImageRoute.js");
+const cartRouter = require("./cartRoute.js");
+const wishListRouter = require("./wishListRoute.js");
 
 const router = express.Router();
 
@@ -14,5 +15,7 @@ router.use(brandRouter);
 router.use(categoryRouter);
 router.use(productImageRouter);
 router.use(cartRouter);
+router.use(wishListRouter);
+
 
 module.exports = router;
--- back-end/server.js
+++ back-end/server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
 // Define the port variable
 const port = 3000;
 
 // MongoDB connection URI
 const dbURI =
-  'mongodb+srv://user123:user123@capstonebackend.o78na.mongodb.net/capstone-backend?retryWrites=true&w=majority';
+  "mongodb+srv://user123:user123@capstonebackend.o78na.mongodb.net/capstone-backend?retryWrites=true&w=majority";
 
 // Connect to MongoDB
 mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(dbURI)
   .then(() => {
     // Start the server after successful connection
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
-  .catch((error) => console.log('Connection error:', error));
+  .catch((error) => console.log("Connection error:", error));
 
 // Define API routes
-app.use('/api', routes);
+app.use("/api", routes);
 
 // Health check endpoint
-app.get('/health', (req, res) => {
-  res.status(200).send('Server is healthy!');
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is healthy!");
 });
 
 // Function to close the mongoose connection
